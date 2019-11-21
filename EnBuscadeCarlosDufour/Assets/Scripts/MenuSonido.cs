6205da6ea7b795039e2f390a78fd9b9039b13def
Franpok <Franpok@users.noreply.github.com>
2019-11-21 07:22:09 +0100

    Menú de sonido implementado en el menú principal, a espera de tener el scroller

---
+++ EnBuscadeCarlosDufour/Assets/Scripts/MenuSonido.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuSonido : MonoBehaviour
+{
+    private void Start()
+    {
+        menuPausa.SetActive(false);
+        audioSrc = GetComponent<AudioSource>();
+    }
+    private AudioSource audioSrc;
+    public float volumen; 
+    public GameObject menuPausa;
+    public Sprite muted, sonido;
+    public GameObject boton;
+    // Start is called before the first frame update
+
+    public void iniciarMenu()
+    {
+     
+        menuPausa.SetActive(true);
+        Time.timeScale = 0;
+       
+    }
+    
+    public void mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        if(AudioListener.pause == true)
+        {
+            boton.GetComponent<Image>().sprite = sonido;
+        }
+        else
+        {
+            boton.GetComponent<Image>().sprite = muted;
+        }
+    }
+    public void cerrarMenu()
+    {
+   
+        menuPausa.SetActive(false);
+        Time.timeScale = 1;
+       
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        volumen = audioSrc.volume;
+    }
+}
