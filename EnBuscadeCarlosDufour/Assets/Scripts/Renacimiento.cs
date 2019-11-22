b4ebc257a5ea4b119aefef7939ac08b4c7c4b85a
Franpok <Franpok@users.noreply.github.com>
2019-11-22 01:49:16 +0100

    Bugs arreglados

---
+++ EnBuscadeCarlosDufour/Assets/Scripts/Renacimiento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Renacimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void iraRenacimiento()
+    {
+        SceneManager.LoadScene(8);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
